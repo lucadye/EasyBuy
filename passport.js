402dcea36fc0de7ea46175e1ef81601b95550d0f
lucadye <121118370+lucadye@users.noreply.github.com>
2024-08-21 13:50:09 -0400

    Complete Google OAuth implementation

--- passport.js
+++ passport.js
@@ -34,7 +34,7 @@ function use(app) {
   passport.use(new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.SERVER_ORIGIN + '/api/auth/google/callback',
+      callbackURL: process.env.CLIENT_ORIGIN+'/auth/google/callback',
       scope: [ 'profile' ],
       session: true,
     }, async (accessToken, refreshToken, profile, done) => {
--- routes/googleAuth.js
+++ routes/googleAuth.js
@@ -2,22 +2,21 @@ const express = require('express');
 const router = express.Router();
 const {passport} = require('../passport.js');
 
-
 router.get('/',
   passport.authenticate('google', {
     scope: ['profile']
   })
 );
 
-
 router.get('/callback',
   passport.authenticate('google', {
-    failureRedirect: '/login'
+    failureRedirect: '/failure'
   }),
   (req, res) => {
-    res.redirect('/');
+    res.redirect(`/api/users/${req.user.id}`);
   }
 );
 
+router.get('/failure', (req, res) => res.sendStatus(400))
 
 module.exports = router;
